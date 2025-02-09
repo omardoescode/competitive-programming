f33158053ebae46b6ac93b9ee4d148c1a859f1d8
omardoescode <omardoescode@gmail.com>
2025-02-09 07:28:42 +0200

    feat: Update template

--- templates/init.cpp
+++ templates/init.cpp
@@ -1,38 +1,61 @@
+/**
+ * بسم الله الرحمن الرحيم
+ * Solved by: Omar Mohammad
+ * Contact Me: https://www.linkedin.com/in/omardoescode/
+ * My Github: https://github.com/omardoescode/
+ */
 #include "bits/stdc++.h"
-#include <climits>
-#include <cmath>
 
 using namespace std;
 
 #define ll long long
 #define str string
-#define oo (int)20000000
+#define ooi (INT_MAX / 2)
+#define ooll (LONG_LONG_MAX / 2)
 #define ull unsigned ll
 #define pdi pair<double, int>
 #define pii pair<int, int>
 #define pdd pair<double, double>
 #define endl "\n"
+#define int long long
 #define in(a)                                                                  \
   int a;                                                                       \
-  scanf("%d", &a);
+  cin >> a;
 #define in2(a, b)                                                              \
   int a, b;                                                                    \
-  scanf("%d%d", &a, &b);
+  cin >> a >> b;
 #define in3(a, b, c)                                                           \
   int a, b, c;                                                                 \
-  scanf("%d%d%d", &a, &b, &c);
+  cin >> a >> b >> c;
+#define cin(a)                                                                 \
+  for (auto &v : a)                                                            \
+  cin >> v
+#define cout(a)                                                                \
+  for (auto &v : a)                                                            \
+    cout << v << " ";                                                          \
+  cout << endl
+#define FASTIO                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL)
 
 #define vec vector
 #define veci vector<int>
 #define vecll vector<ll>
 #define all(v) v.begin(), v.end()
 
-void solve() {
-  // Code here
-}
+void precompute() {}
+
+void solve() {}
+
 signed main() {
+  FASTIO;
+  // #ifndef ONLINE_JUDGE
+  //   freopen("input.txt", "r", stdin);
+  //   // freopen("output.txt", "w", stdout);
+  // #endif
   int t = 1, c = 0;
-  // scanf("%d", &t);
+  cin >> t;
+  precompute();
   while (t--)
     solve();
 }
