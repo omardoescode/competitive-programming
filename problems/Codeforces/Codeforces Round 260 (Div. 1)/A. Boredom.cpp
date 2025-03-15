4278ea054f19930ae8b073c7f48c138cc8c6f6d7
omardoescode <omardoescode@gmail.com>
2025-03-15 11:37:50 +0200

    Boredom

---
+++ problems/Codeforces/Codeforces Round 260 (Div. 1)/A. Boredom.cpp
@@ -0,0 +1,104 @@
+/**
+ * بسم الله الرحمن الرحيم
+ * Solved by: Omar Mohammad
+ * Contact Me: https://www.linkedin.com/in/omardoescode/
+ * My Github: https://github.com/omardoescode/
+ *
+ * Problem: A. Boredom
+ * Contest: Codeforces Round 260 (Div. 1)
+ * Judge: Codeforces
+ * URL: https://codeforces.com/problemset/problem/455/A
+ * Memory Limit: 256
+ * Time Limit: 1000
+ * Start: Sat 15 Mar 2025 11:13:51 AM EET
+ */
+#include "bits/stdc++.h"
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define int ll
+#define str string
+#define ooi (INT_MAX / 2)
+#define ooll (LONG_LONG_MAX / 2)
+#define ull unsigned ll
+#define pdi pair<double, int>
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define endl "\n"
+#define in(a)                                                                  \
+  int a;                                                                       \
+  cin >> a;
+#define in2(a, b)                                                              \
+  int a, b;                                                                    \
+  cin >> a >> b;
+#define in3(a, b, c)                                                           \
+  int a, b, c;                                                                 \
+  cin >> a >> b >> c;
+#define cin(a)                                                                 \
+  for (auto &v : a)                                                            \
+  cin >> v
+#define cout(a)                                                                \
+  for (auto &v : a)                                                            \
+    cout << v << " ";                                                          \
+  cout << endl
+#define FASTIO                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL)
+
+#define v vector
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long>>
+#define vi vector<int>
+#define vll vector<ll>
+#define all(v) (v).begin(), (v).end()
+
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define __lcm(a, b) ((a * b) / (__gcd(a, b)))
+#define unq(v) (v).erase(unique(all((v))), (v).end())
+
+void precompute() {}
+
+const int N = 1e5 + 100;
+vector<int> vec;
+int freq[N];
+int dp[N], n;
+int solve(int i) {
+  if (i == n)
+    return 0;
+  int &ret = dp[i];
+  if (~ret)
+    return ret;
+  int leave = solve(upper_bound(all(vec), vec[i]) - vec.begin());
+  int take = vec[i] * freq[vec[i]] +
+             solve(upper_bound(all(vec), vec[i] + 1) - vec.begin());
+  return ret = max(leave, take);
+}
+void solve() {
+  cin >> n;
+  memset(dp, -1, sizeof(dp));
+  memset(freq, 0, sizeof(freq));
+  vec.resize(n);
+  cin(vec);
+  sort(all(vec));
+  for (int i = 0; i < n; i++)
+    freq[vec[i]]++;
+  unq(vec);
+  n = vec.size();
+  cout << solve(0) << endl;
+}
+
+signed main() {
+  FASTIO;
+  // #ifndef ONLINE_JUDGE
+  //   freopen("input.txt", "r", stdin);
+  //   // freopen("output.txt", "w", stdout);
+  // #endif
+  int t = 1, c = 0;
+  // cin >> t;
+  precompute();
+  while (t--)
+    solve();
+}
