973c5175583530f31eb34b643ad02214587d7ad3
omardoescode <omardoescode@gmail.com>
2025-03-15 09:48:04 +0200

    Frog 1 and 2

--- problems/AtCoder/Educational DP Contest/B - Frog 2.cpp
+++ problems/AtCoder/Educational DP Contest/B - Frog 2.cpp
@@ -4,13 +4,13 @@
  * Contact Me: https://www.linkedin.com/in/omardoescode/
  * My Github: https://github.com/omardoescode/
  *
- * Problem: A - Frog 1
+ * Problem: B - Frog 2
  * Contest: Educational DP Contest
  * Judge: AtCoder
- * URL: https://atcoder.jp/contests/dp/tasks/dp_a
+ * URL: https://atcoder.jp/contests/dp/tasks/dp_b
  * Memory Limit: 1024
  * Time Limit: 2000
- * Start: Sun 09 Mar 2025 07:40:07 AM EET
+ * Start: Sun 09 Mar 2025 12:34:52 PM EET
  */
 #include "bits/stdc++.h"
 #include <climits>
@@ -18,7 +18,6 @@
 using namespace std;
 
 #define ll long long
-#define int ll
 #define str string
 #define ooi (INT_MAX / 2)
 #define ooll (LONG_LONG_MAX / 2)
@@ -46,7 +45,6 @@ using namespace std;
 #define FASTIO                                                                 \
   ios_base::sync_with_stdio(false);                                            \
   cin.tie(NULL)
-
 #define v vector
 #define vvi vector<vector<int>>
 #define vvl vector<vector<long long>>
@@ -58,11 +56,11 @@ using namespace std;
 #define for1(i, n) for (int i = 1; i <= n; i++)
 #define __lcm(a, b) ((a * b) / (__gcd(a, b)))
 #define unq(v) (v).erase(unique(all((v)), (v).end()))
-
 void precompute() {}
+#define int ll
 
 const int N = 10e5 + 10;
-int n;
+int n, k;
 int heights[N];
 
 int dp[N];
@@ -74,17 +72,17 @@ int min_value(int i) {
   if (~ret)
     return ret;
 
-  int step1 = LLONG_MAX, step2 = LLONG_MAX;
-  step1 = abs(heights[i] - heights[i + 1]) + min_value(i + 1);
+  ret = LLONG_MAX;
+  for1(l, k) {
+    if (i + l > n - 1)
+      break;
+    ret = min(ret, abs(heights[i] - heights[i + l]) + min_value(i + l));
+  }
 
-  // still can jump to N?
-  if (i + 2 <= n - 1)
-    step2 = abs(heights[i] - heights[i + 2]) + min_value(i + 2);
-
-  return ret = min(step1, step2);
+  return ret;
 }
 void solve() {
-  cin >> n;
+  cin >> n >> k;
   memset(dp, -1, sizeof(dp));
   for0(i, n) cin >> heights[i];
   cout << min_value(0) << endl;
